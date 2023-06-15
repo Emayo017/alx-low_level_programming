cdbea0700e75ba014c4290471d492a6d9efaf74b
Emayo017 <jimohemayo@yahoo.com>
2023-06-15 14:06:26 -0700

    now

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include<stdio.h>
+
+/**
+ *main - Printing with printf
+ *Description: print with printf
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
