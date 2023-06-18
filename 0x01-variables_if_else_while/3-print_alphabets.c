9d956d9024f4c38b8ee03b308ca940daac373e80
Emayo017 <jimohemayo@yahoo.com>
2023-06-17 23:51:15 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - UPPER and lower case letter
+ *
+ * description - This is for UPPER and lower case letter
+ * return (0);
+ */
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	for (letter <= 'z')
+	putchar(lowercase);
+		lowercase++
+	for (uppercase <= 'Z')
+	putchar(uppercase);
+		uppercase++;
+	putchar('\n');
+	return (0);
+}
