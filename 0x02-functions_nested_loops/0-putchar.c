c90f531451779fa74b213b0e20ec2cbf1dd3bbb4
Emayo017 <jimohemayo@yahoo.com>
2023-06-20 15:01:25 -0700

    todya

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - This program prints putchar
+ *
+ * description - Print putchar
+ * return (0)
+ */
+int main(void)
+{
+	int putchar(char c);
+	{
+		_putchar('-');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r'\n);
+	}
+return 0;
+}
